208f515c3e6df16eb81726cad801bfce3313f10d
User <ulyanovskiy.2001@mail.ru>
2025-11-26 21:31:02 +0300

    feat: create basic demo logic

---
+++ Npgsql.EFCore.Tracker.Api.Demo/Database/DatabaseContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Npgsql.EFCore.Tracker.Api.Demo.Database.Entities;
+using Npgsql.EFCore.Tracker.Core.Extensions;
+
+namespace Npgsql.EFCore.Tracker.Api.Demo.Database;
+
+public sealed class DatabaseContext : DbContext
+{
+    public DbSet<Role> Roles => Set<Role>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Role>(builder =>
+        {
+            builder.HasKey(c => c.Id);
+            builder.EnableTracking();
+        });
+    }
+}
--- Npgsql.EFCore.Tracker.Api.Demo/Program.cs
+++ Npgsql.EFCore.Tracker.Api.Demo/Program.cs
@@ -1,23 +1,36 @@
+using Microsoft.EntityFrameworkCore;
+using Npgsql.EFCore.Tracker.Api.Demo.Database;
+using Npgsql.EFCore.Tracker.Core.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    builder.Services.AddControllers();
+
+    builder.Services.AddDbContext<DatabaseContext>(options =>
+    {
+        options.UseNpgsql(
+            "Host=localhost;Port=5432;Database=HubDb;Username=postgres;Password=qwerty",
+            npgsql =>
+            {
+                npgsql.EnableTableTrackingSupport();
+            });
+    });
+
+    builder.Services.AddOpenApi();
+
 }
 
-app.UseHttpsRedirection();
+var app = builder.Build();
+{
+    if (app.Environment.IsDevelopment())
+    {
+        app.MapOpenApi();
+    }
 
-app.UseAuthorization();
+    app.UseHttpsRedirection();
 
-app.MapControllers();
+    app.UseAuthorization();
 
+    app.MapControllers();
+}
 app.Run();
