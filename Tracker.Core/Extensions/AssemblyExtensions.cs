85257853d291091b3ff7b088d24c59ea4cd01f61
User <ulyanovskiy.2001@mail.ru>
2025-12-02 11:21:46 +0300

    feat: assembly write time extension

---
+++ Tracker.Core/Extensions/AssemblyExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+
+namespace Tracker.Core.Extensions;
+
+public static class AssemblyExtensions
+{
+    public static DateTimeOffset GetAssemblyWriteTime(this Assembly assembly)
+    {
+        ArgumentNullException.ThrowIfNull(assembly, nameof(assembly));
+
+        if (string.IsNullOrEmpty(assembly.Location))
+            throw new InvalidOperationException(
+                $"Cannot determine write time for assembly '{assembly.FullName}' " +
+                "because it has no physical location (may be dynamic or in-memory).");
+
+        var assemblyPath = assembly.Location;
+
+        if (!File.Exists(assemblyPath))
+            throw new FileNotFoundException(
+                $"Cannot determine write time for assembly '{assembly.FullName}'. " +
+                $"Assembly file not found at '{assemblyPath}'");
+
+        var lastWriteTimeUtc = File.GetLastWriteTimeUtc(assemblyPath);
+        return new DateTimeOffset(lastWriteTimeUtc);
+    }
+}
