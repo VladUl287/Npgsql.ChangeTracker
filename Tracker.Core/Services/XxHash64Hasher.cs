42437e6a5ff87d0efe70723260ecdddbe9e5fe80
User <ulyanovskiy.2001@mail.ru>
2025-12-10 10:31:19 +0300

    refactor: split little endian and big endian logic to methods

--- Tracker.Core/Services/XxHash64Hasher.cs
+++ Tracker.Core/Services/XxHash64Hasher.cs
@@ -10,30 +10,42 @@ public sealed class XxHash64Hasher : ITimestampsHasher
 {
     public ulong Hash(ReadOnlySpan<DateTimeOffset> timestamps)
     {
+        if (BitConverter.IsLittleEndian)
+            return HashLittleEndian(timestamps);
+
+        return HashBigEndian(timestamps);
+    }
+
+    private static ulong HashLittleEndian(ReadOnlySpan<DateTimeOffset> timestamps)
+    {
         const int StackAllocThreshold = 128;
 
         var byteCount = timestamps.Length * sizeof(long);
 
-        if (BitConverter.IsLittleEndian)
+        if (byteCount > StackAllocThreshold)
         {
-            if (byteCount > StackAllocThreshold)
-            {
-                var rented = ArrayPool<long>.Shared.Rent(timestamps.Length);
-                Span<long> ticksBuffer = rented.AsSpan(0, timestamps.Length);
+            var rented = ArrayPool<long>.Shared.Rent(timestamps.Length);
+            Span<long> ticksBuffer = rented.AsSpan(0, timestamps.Length);
 
-                for (int i = 0; i < timestamps.Length; i++)
-                    ticksBuffer[i] = timestamps[i].Ticks;
-
-                var hash = XxHash64.HashToUInt64(MemoryMarshal.AsBytes(ticksBuffer));
-                ArrayPool<long>.Shared.Return(rented);
-                return hash;
-            }
-
-            Span<long> ticks = stackalloc long[timestamps.Length];
             for (int i = 0; i < timestamps.Length; i++)
-                ticks[i] = timestamps[i].Ticks;
-            return XxHash64.HashToUInt64(MemoryMarshal.AsBytes(ticks));
+                ticksBuffer[i] = timestamps[i].Ticks;
+
+            var hash = XxHash64.HashToUInt64(MemoryMarshal.AsBytes(ticksBuffer));
+            ArrayPool<long>.Shared.Return(rented);
+            return hash;
         }
+
+        Span<long> ticks = stackalloc long[timestamps.Length];
+        for (int i = 0; i < timestamps.Length; i++)
+            ticks[i] = timestamps[i].Ticks;
+        return XxHash64.HashToUInt64(MemoryMarshal.AsBytes(ticks));
+    }
+
+    private static ulong HashBigEndian(ReadOnlySpan<DateTimeOffset> timestamps)
+    {
+        const int StackAllocThreshold = 128;
+
+        var byteCount = timestamps.Length * sizeof(long);
 
         if (byteCount > StackAllocThreshold)
         {
