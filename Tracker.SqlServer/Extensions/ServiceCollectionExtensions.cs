99b62dadccbfeb99b1be044011797714ccb5b71e
User <ulyanovskiy.2001@mail.ru>
2025-12-18 17:16:07 +0300

    feat: add tracking modes (index stats, change tracking) for sql server

--- Tracker.SqlServer/Extensions/ServiceCollectionExtensions.cs
+++ Tracker.SqlServer/Extensions/ServiceCollectionExtensions.cs
@@ -1,23 +1,30 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Tracker.Core.Services.Contracts;
+using Tracker.SqlServer.Models;
 using Tracker.SqlServer.Services;
 
 namespace Tracker.SqlServer.Extensions;
 
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddSqlServerSource(this IServiceCollection services, string sourceId, string connectionString)
+    public static IServiceCollection AddSqlServerSource(
+        this IServiceCollection services, string sourceId, string connectionString, TrackingMode mode = default)
     {
         ArgumentException.ThrowIfNullOrEmpty(sourceId);
         ArgumentException.ThrowIfNullOrEmpty(connectionString);
 
-        return services.AddSingleton<ISourceOperations>((_) =>
-            new SqlServerIndexUsageOperations(sourceId, connectionString)
-        );
+        Func<IServiceProvider, ISourceOperations> factory = mode switch
+        {
+            TrackingMode.DbIndexUsageStats => (_) => new SqlServerIndexUsageOperations(sourceId, connectionString),
+            TrackingMode.ChangeTracking => (_) => new SqlServerChangeTrackingOperations(sourceId, connectionString),
+            _ => throw new InvalidOperationException()
+        };
+
+        return services.AddSingleton(factory);
     }
 
-    public static IServiceCollection AddSqlServerSource<TContext>(this IServiceCollection services)
+    public static IServiceCollection AddSqlServerSource<TContext>(this IServiceCollection services, TrackingMode mode = default)
          where TContext : DbContext
     {
         return services.AddSingleton<ISourceOperations>((provider) =>
@@ -31,11 +38,16 @@ public static class ServiceCollectionExtensions
             var sourceIdGenerator = scope.ServiceProvider.GetRequiredService<ISourceIdGenerator>();
             var sourceId = sourceIdGenerator.GenerateId<TContext>();
 
-            return new SqlServerIndexUsageOperations(sourceId, connectionString);
+            return mode switch
+            {
+                TrackingMode.DbIndexUsageStats => new SqlServerIndexUsageOperations(sourceId, connectionString),
+                TrackingMode.ChangeTracking => new SqlServerChangeTrackingOperations(sourceId, connectionString),
+                _ => throw new InvalidOperationException()
+            };
         });
     }
 
-    public static IServiceCollection AddSqlServerSource<TContext>(this IServiceCollection services, string sourceId)
+    public static IServiceCollection AddSqlServerSource<TContext>(this IServiceCollection services, string sourceId, TrackingMode mode = default)
          where TContext : DbContext
     {
         ArgumentException.ThrowIfNullOrEmpty(sourceId, nameof(sourceId));
@@ -48,7 +60,12 @@ public static class ServiceCollectionExtensions
             var connectionString = dbContext.Database.GetConnectionString() ??
                 throw new NullReferenceException($"Connection string is not found for context {typeof(TContext).FullName}.");
 
-            return new SqlServerIndexUsageOperations(sourceId, connectionString);
+            return mode switch
+            {
+                TrackingMode.DbIndexUsageStats => new SqlServerIndexUsageOperations(sourceId, connectionString),
+                TrackingMode.ChangeTracking => new SqlServerChangeTrackingOperations(sourceId, connectionString),
+                _ => throw new InvalidOperationException()
+            };
         });
     }
 }
---
+++ Tracker.SqlServer/Models/TrackingMode.cs
@@ -0,0 +1,15 @@
+﻿namespace Tracker.SqlServer.Models;
+
+public enum TrackingMode
+{
+    /// <summary>
+    /// <c>sys.dm_db_index_usage_stats</c> returns counts of different types of index operations and the time each type of operation was last performed.
+    /// <a href="https://learn.microsoft.com/en-us/sql/relational-databases/system-dynamic-management-views/sys-dm-db-index-usage-stats-transact-sql">Source ID Documentation</a>.
+    /// </summary>
+    DbIndexUsageStats,
+    /// <summary>
+    /// Change Tracking lightweight solution that provides an efficient change tracking mechanism.
+    /// <a href="https://learn.microsoft.com/en-us/sql/relational-databases/track-changes/about-change-tracking-sql-server">Source ID Documentation</a>.
+    /// </summary>
+    ChangeTracking
+}
