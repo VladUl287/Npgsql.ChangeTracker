e875205c29f979f557e626d5495e9cb697da57c1
User <ulyanovskiy.2001@mail.ru>
2025-12-24 21:49:08 +0300

    refactor: combine ulong extensions into one class

--- Tracker.Core/Extensions/UlongExtensions.cs
+++ Tracker.Core/Extensions/UlongExtensions.cs
@@ -2,9 +2,23 @@
 
 internal static class UlongExtensions
 {
-    internal static int CountDigits(this ulong n)
+    private const int ULongMaxLength = 20;
+
+    internal static bool MatchesULong(this ReadOnlySpan<char> chars, ulong number)
     {
-        return n switch
+        if (chars is { Length: 0 or > ULongMaxLength })
+            return false;
+
+        ulong result = 0;
+        foreach (var c in chars)
+            result = result * 10 + (uint)(c - '0');
+
+        return result == number;
+    }
+
+    internal static int CountDigits(this ulong number)
+    {
+        return number switch
         {
             < 10UL => 1,
             < 100UL => 2,
